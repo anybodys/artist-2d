c0deb5de11f2f69025bf95dfdb8bb2e5f3912a9a
K Davis <anybodys@users.noreply.github.com>
2022-01-01 13:04:27 -0800

    Age based on DNA. (#18)

--- artist/painter.py
+++ artist/painter.py
@@ -4,27 +4,24 @@ from artist import stroke
 class Painter:
 
   def __init__(self, dna, graphics_engine):
-    self.age = 0
+    self.age = 0.0
     self.raw_dna = str(dna)
     self.graphics_engine = graphics_engine
-    self.chromosomes = [
-      dna_parser.Chromosome(chromosome_str.strip(), self.graphics_engine)
-      for chromosome_str in self.raw_dna.split('\n')
-    ]
+    self.chromosomes = []
+    for chromosome_str in self.raw_dna.split('\n'):
+      c = dna_parser.Chromosome(chromosome_str.strip(), self.graphics_engine)
+      if c.is_valid():
+        self.chromosomes.append(c)
+    self.max_age = self.chromosomes[0].post_junk_fun()
 
   def still_growing(self):
-    # TODO(kmd): Make max age based on DNA.
-    if self.age > 3:
-      return False
-    return True
+    return self.age <= self.max_age
 
   def age_up(self):
-    self.age += 1
+    self.age += self.chromosomes[0].pre_junk_fun()
 
   def paint(self):
     for c in self.chromosomes:
-      if not c.is_valid():
-        continue
       # TODO(kmd): Generate the genes for the inverse side.
       next_stroke = stroke.Stroke(c.pre_junk_fun, c.post_junk_fun)
       for g in dna_parser.GeneSequencer(c):
