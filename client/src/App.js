60b0e73b2511598d138a7b622f0381f064f0955a
kmd <anybodys@gmail.com>
2024-02-17 14:18:50 -0800

    Add basic app to display mock data.

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Museum/>
+    </div>
+  );
+}
+
+
+class Museum extends React.Component {
+    render() {
+        // TODO(kmd): This should come from the API. :)
+        const paintingsData = [{
+            publicLink: 'https://storage.googleapis.com/artist-2d.appspot.com/gen-0/0427b526-d632-41a1-b995-1b07a24befba.jpg',
+            generation: 0,
+            artistId: 'example-artist-id-1'
+        },{
+            publicLink: 'https://storage.googleapis.com/artist-2d.appspot.com/gen-0/0427b526-d632-41a1-b995-1b07a24befba.jpg',
+            generation: 0,
+            artistId: 'example-artist-id-2'
+        }];
+
+        const paintings = paintingsData.map(pd =>
+            this.renderPainting(pd)
+        );
+
+        return (<div>{paintings}</div>);
+    }
+
+
+    renderPainting(pd) {
+        return (
+                <Painting
+            key={pd.artistId}
+            props={pd}
+                />
+        );
+    }
+}
+
+
+class Painting extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            publicUrl: props.props.publicLink
+        };
+    }
+
+    render() {
+        return (
+                <img className="Painting"src={this.state.publicUrl} />
+        );
+    }
+}
+
+export default App;
