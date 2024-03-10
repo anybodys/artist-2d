381a75e9008e9a3b8eb31d4f8b8893973cd79b52
kmd <anybodys@gmail.com>
2024-03-10 14:23:58 -0700

    Configure openapi for google endpoints.

--- voting_api/voting/api.py
+++ voting_api/voting/api.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 from apiflask import APIFlask
 from flask import jsonify, request
@@ -7,11 +7,27 @@ from voting.storage import art_storage
 
 
 def create_app():
-  app = APIFlask(__name__)
+  app = APIFlask(__name__, title='Artist 2D Voting API', spec_path='/openapi.yaml')
   return app
 
 
 app = create_app()
+app.config.update({
+  'SPEC_FORMAT': 'yaml',
+  'OPENAPI_VERSION': '2.0',
+  'SYNC_LOCAL_SPEC': True,
+  'LOCAL_SPEC_PATH': Path(app.root_path) / 'openapi.yaml',
+})
+
+
+@app.spec_processor
+def google_endpoints_spec(spec):
+    spec['host'] = 'gateway-c24bw3cnyq-wl.a.run.app'
+    spec['x-google-backend'] = {
+      'address': 'voting-api',
+      'protocol': 'h2',
+    }
+    return spec
 
 
 @app.get("/health")
