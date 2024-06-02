0d85a291d204c5a1b51604494db4112b209933e5
K Davis <anybodys@users.noreply.github.com>
2024-06-01 19:07:35 -0700

    Call actual API for painting data. (#60)
    * Call actual API for painting data.
    
    * Add simple CORS headers

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders image', () => {
+
+jest.mock("./api", () => {
+  const VotingApi = {
+    get: async function () {
+      console.log('here');
+      return ({art: [{
+        artist_id: "mock-artist-id",
+        generation: "0",
+        public_link: "mock-art-url"
+      }]});
+    }
+  };
+  return {VotingApi}
+});
+
+test('renders image', async () => {
   render(<App />);
-  const imgElement = screen.getByAltText(/.*example-artist-id-1/);
+  const imgElement = await screen.findByAltText(/.*mock-artist-id/);
   expect(imgElement).toBeInTheDocument();
+  expect(imgElement.src).toBe('http://localhost/mock-art-url')
 });
