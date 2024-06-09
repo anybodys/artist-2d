4ee874f16ad28030baabd9b9a6f573e686cbbd81
K Davis <anybodys@users.noreply.github.com>
2024-06-09 09:19:59 -0700

    Add db. (#63)

--- storageapi/api/models.py
+++ storageapi/api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+def get_current_generation():
+  # Get the max id of where `active_date` is not `Null`.
+  gen = (Generation.objects
+         .filter(active_date__isnull=False)
+         .order_by("-id")
+         .values_list("id", flat=True)[0])
+  return gen
+
+
+class DatetimeManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset().filter(deleted_date__isnull=True)
+
+class Datetime(models.Model):
+  class Meta:
+    abstract = True
+
+  objects = DatetimeManager()
+
+  created_date = models.DateTimeField(auto_now_add=True)
+  updated_date = models.DateTimeField(auto_now=True)
+  deleted_date = models.DateTimeField(null=True)
+
+class Generation(Datetime):
+  id = models.AutoField(primary_key=True)
+  active_date = models.DateTimeField(null=True)
+  inaction_date = models.DateTimeField(null=True)
+
+  def __str__(self):
+    return f'{self.id}: {self.active_date} - {self.inaction_date}'
--- storageapi/api/tests.py
+++ storageapi/api/tests.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from django.test import TestCase
 from django.test.client import Client
 
@@ -6,8 +8,32 @@ class TestApi(TestCase):
   def setUp(self):
     self.client = Client()
 
+    art_storage_patch = mock.patch('api.art_storage.ArtStorage')
+    self.art_storage = art_storage_patch.start().return_value
+    self.addCleanup(art_storage_patch.stop)
+
   def test_health(self):
     response = self.client.get('/api/health')
 
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content, b'OK')
+
+  def test_art_get(self):
+    self.art_storage.get_art.return_value = {}
+
+    response = self.client.get('/api/art')
+
+    # No params should use the current gen in the DB.
+    self.art_storage.get_art.assert_called_with(1)
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json(), {})
+
+  def test_art_get_query_param_gen(self):
+    self.art_storage.get_art.return_value = {}
+
+    response = self.client.get('/api/art?gen=19')
+
+    # No params should use the current gen in the DB.
+    self.art_storage.get_art.assert_called_with(19)
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json(), {})
