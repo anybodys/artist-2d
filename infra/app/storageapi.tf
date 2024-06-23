404380b11b453a7bfdda41bce000947e27fce2d3
K Davis <anybodys@users.noreply.github.com>
2024-06-23 11:45:03 -0700

    Move storageapi to own file. (#80)

---
+++ infra/app/storageapi.tf
@@ -0,0 +1,115 @@
+################################################################
+## Storage API
+################################################################
+
+locals {
+  storageapi_image = docker_registry_image.app["storageapi"].name
+}
+
+resource "google_service_account" "storageapi" {
+  account_id   = "cloud-run-service-account"
+  display_name = "Service account for Cloud Run Storage API"
+}
+
+resource "google_cloud_run_v2_service" "storageapi" {
+  name     = "storageapi"
+  location = var.region
+  ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
+  template {
+
+    volumes {
+      name = "cloudsql"
+      cloud_sql_instance {
+        instances = [google_sql_database_instance.artist2d.connection_name]
+      }
+    }
+
+    containers {
+      name  = "storageapi"
+      image = local.storageapi_image
+
+      ports {
+        container_port = 8000
+      }
+
+      env {
+        name  = "ENV"
+        value = "prod"
+      }
+      env {
+        name  = "GOOGLE_CLOUD_PROJECT"
+        value = var.project
+      }
+      env {
+        name  = "POSTGRES_HOST"
+        value = "/cloudsql/${var.project}:${var.region}:${google_sql_database_instance.artist2d.name}"
+      }
+      env {
+        name = "SECRET_KEY"
+        value_source {
+          secret_key_ref {
+            secret  = "django-secret-key"
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name  = "POSTGRES_USER"
+        value = "storageapi"
+      }
+      env {
+        name = "POSTGRES_PASSWORD"
+        value_source {
+          secret_key_ref {
+            secret  = "storageapi-db-pass"
+            version = "latest"
+          }
+        }
+      }
+      volume_mounts {
+        name       = "cloudsql"
+        mount_path = "/cloudsql"
+      }
+    }
+    service_account = google_service_account.storageapi.email
+  }
+}
+
+resource "google_secret_manager_secret_iam_member" "storage-db-api" {
+  secret_id = google_secret_manager_secret.storageapi-db-pass.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.storageapi.email}"
+}
+
+resource "google_project_iam_member" "cloudsql" {
+  project = var.project
+  role    = "roles/cloudsql.client"
+  member  = "serviceAccount:${google_service_account.storageapi.email}"
+}
+
+## Django Secret Key secret.
+
+resource "random_password" "django-secret-key" {
+  length = 64
+}
+
+resource "google_secret_manager_secret" "django-secret-key" {
+  secret_id = "django-secret-key"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "django-secret-current" {
+  secret = google_secret_manager_secret.django-secret-key.id
+
+  secret_data = random_password.django-secret-key.result
+}
+
+resource "google_secret_manager_secret_iam_member" "django-secret-key" {
+  secret_id = google_secret_manager_secret.django-secret-key.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.storageapi.email}"
+}
