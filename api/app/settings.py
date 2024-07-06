92add036be1113452524da57febdda4b549a77a7
kmd <anybodys@gmail.com>
2024-07-05 23:12:23 -0700

    Set CORS headers.

--- api/app/settings.py
+++ api/app/settings.py
@@ -39,6 +39,15 @@ else:
   ALLOWED_HOSTS = ["*"]
 
 
+CORS_ALLOWED_ORIGINS = [
+  # Prod
+  "https://kmdcodes.com",
+  # Local
+  "http://localhost:3000",
+  "http://127.0.0.1:3000",
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -48,12 +57,16 @@ INSTALLED_APPS = [
   'django.contrib.sessions',
   'django.contrib.messages',
   'django.contrib.staticfiles',
+
+  'corsheaders',
+
   'api',
 ]
 
 MIDDLEWARE = [
   'django.middleware.security.SecurityMiddleware',
   'django.contrib.sessions.middleware.SessionMiddleware',
+  'corsheaders.middleware.CorsMiddleware',
   'django.middleware.common.CommonMiddleware',
   #'django.middleware.csrf.CsrfViewMiddleware',
   'django.contrib.auth.middleware.AuthenticationMiddleware',
